b73bd2dada3f5e8fbb94ed05e0ae7b41112767fd
Marina Bragatti Barbosa <marinabragatti@hotmail.com>
2020-01-14 10:24:33 -0300

    Desafio 01 em C#

---
+++ Projeto/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo carro = new Veiculo();
+
+            int opcao = 0;
+            int qtdeLitros = 0;
+            String cor;
+            String opcao1;
+
+            do
+            {
+                Console.WriteLine("\n----------------------MENU-----------------------");
+                Console.WriteLine("1 - Ligar");
+                Console.WriteLine("2 - Desligar");
+                Console.WriteLine("3 - Acelerar");
+                Console.WriteLine("4 - Frear");
+                Console.WriteLine("5 - Abastecer");
+                Console.WriteLine("6 - Pintar");
+                Console.WriteLine("7 - Sair");
+
+                Console.WriteLine("Escolha uma opção: ");
+                opcao1 = Console.ReadLine();
+                opcao = Convert.ToInt32(opcao1);
+
+                switch (opcao)
+                {
+                    case 1:
+                        carro.ligar();
+                        break;
+                    case 2:
+                        carro.desligar();
+                        break;
+                    case 3:
+                        carro.acelerar();
+                        break;
+                    case 4:
+                        carro.frear();
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Quantos litros você deseja abastecer? ");
+                            if(int.TryParse(Console.ReadLine(), out qtdeLitros))
+                                carro.abastecer(qtdeLitros);
+                            else
+                                Console.WriteLine("Valor inválido. Insira um valor inteiro.");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Qual cor você deseja pintar seu carro? ");
+                            cor = Console.ReadLine();
+                            carro.pintar(cor);
+                            break;
+                        }
+                    case 7:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+            } while (opcao != 7);
+        }
+    }
+}
---
+++ Projeto/Veiculo.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto
+{
+    class Veiculo
+    {
+        private String marca { get; set; }
+        private String modelo { get; set; }
+        private String placa { get; set; }
+        private String cor { get; set; }
+        private float km { get; set; }
+        private Boolean isLigado { get; set; }
+        private int litrosCombustivel { get; set; }
+        private int velocidade { get; set; }
+        private double preco { get; set; }
+
+        //Métodos
+
+        public void ligar()
+        {
+            if (this.isLigado)
+                Console.WriteLine("O carro já está ligado");
+            else
+            {
+                this.isLigado = true;
+                Console.WriteLine("Você ligou o carro.");
+            }
+        }
+
+        public void desligar()
+        {
+            if (this.isLigado)
+            {
+                this.isLigado = false;
+                Console.WriteLine("Você desligou o carro");
+            }
+            else
+                Console.WriteLine("O carro já está desligado.");
+        }
+
+        public void acelerar()
+        {
+            if (this.isLigado && this.velocidade <= 140 && this.litrosCombustivel > 0)
+            {
+                this.velocidade += 20;
+                this.litrosCombustivel -= 1;
+                Console.WriteLine("Você está a " + this.velocidade + "km/h");
+                Console.WriteLine("Seu carro tem " + this.litrosCombustivel + " litros de combustível");
+            }
+            else if(this.isLigado == false)
+                Console.WriteLine("Ligue o carro para acelerar");
+            else if(this.litrosCombustivel <= 0)
+                Console.WriteLine("O carro não tem combustível, abasteça.");
+            else
+                Console.WriteLine("Você está acima do limite de velocidade, não é possível acelerar mais.");
+        }
+
+        public void frear()
+        {
+            if(this.isLigado && this.velocidade > 0)
+            {
+                this.velocidade -= 10;
+                Console.WriteLine("Seu carro agora está a " + this.velocidade + "km/h");
+            }
+            else if(this.isLigado == false)
+                Console.WriteLine("Seu carro está desligado.");
+            else
+                Console.WriteLine("Seu carro está parado.");
+        }
+
+        public void abastecer(int qtdeLitros)
+        {
+            if(qtdeLitros <= 0)
+                Console.WriteLine("Não é possível abastecer um valor inferior a 1 litro");
+            else if(this.isLigado)
+                Console.WriteLine("Desligue o carro para abastecer");
+            else if(this.velocidade > 0)
+                Console.WriteLine("O carro está em movimento, pare o carro para abastecer");
+            else if((this.litrosCombustivel + qtdeLitros) > 100)
+                Console.WriteLine("Não é possível abastecer, faltam " + (this.velocidade + qtdeLitros - 100) + " para completar o tanque");
+            else
+            {
+                this.litrosCombustivel += qtdeLitros;
+                Console.WriteLine("O tanque tem " + this.litrosCombustivel + " litros.");
+            }
+        }
+
+        public void pintar(String cor)
+        {
+            this.cor = cor;
+            Console.WriteLine("Seu carro agora é " + this.cor);
+        }
+    }
+}
